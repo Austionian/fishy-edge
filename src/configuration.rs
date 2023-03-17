a29c7747d011a1b86f1fbb12e437aa03a7016939
Austin Rooks <Austionian@users.noreply.github.com>
2023-03-16 21:14:17 -0500

    update deps

--- src/configuration.rs
+++ src/configuration.rs
@@ -52,20 +52,28 @@ impl DataBaseSettings {
 }
 
 pub fn get_configuration() -> Result<Settings, config::ConfigError> {
-    let mut settings = config::Config::default();
     let base_path = std::env::current_dir().expect("Failed to determine the current directory.");
     let config_directory = base_path.join("config");
-
-    settings.merge(config::File::from(config_directory.join("base")).required(true))?;
 
     let env: Environment = std::env::var("APP_ENVIRONMENT")
         .unwrap_or_else(|_| "local".into())
         .try_into()
         .expect("Failed to parse APP_ENVIRONMENT.");
 
-    settings.merge(config::File::from(config_directory.join(env.as_str())).required(true))?;
-    settings.merge(config::Environment::with_prefix("app").separator("__"))?;
-    settings.try_into()
+    let environment_filename = format!("{}.yaml", env.as_str());
+    let settings = config::Config::builder()
+        .add_source(config::File::from(config_directory.join("base.yaml")))
+        .add_source(config::File::from(
+            config_directory.join(environment_filename),
+        ))
+        .add_source(
+            config::Environment::with_prefix("APP")
+                .prefix_separator("_")
+                .separator("__"),
+        )
+        .build()?;
+
+    settings.try_deserialize::<Settings>()
 }
 
 pub enum Environment {
--- src/main.rs
+++ src/main.rs
@@ -11,9 +11,7 @@ async fn main() -> std::io::Result<()> {
 
     // Get config and connect to Postgres
     let config = get_configuration().expect("Failed to read configuration.");
-    let connection_pool = PgPoolOptions::new()
-        .connect_timeout(std::time::Duration::from_secs(2))
-        .connect_lazy_with(config.database.with_db());
+    let connection_pool = PgPoolOptions::new().connect_lazy_with(config.database.with_db());
     let address = format!("{}:{}", config.application.host, config.application.port);
     let listener = TcpListener::bind(address)?;
     run(listener, connection_pool)?.await?;
