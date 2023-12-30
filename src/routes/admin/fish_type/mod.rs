8581ebf24740a2181b0df062693e3a37b3a0a3a0
Austin Rooks <Austionian@users.noreply.github.com>
2023-12-29 21:13:10 -0600

    clippy

--- src/routes/admin/fish_type/mod.rs
+++ src/routes/admin/fish_type/mod.rs
@@ -4,7 +4,7 @@ mod read_all;
 mod update;
 mod update_image;
 
-pub use create::{create_fish_type, NewFishType};
+pub use create::create_fish_type;
 pub use read::read_fish_type;
 pub use read_all::read_all_fish_types;
 pub use update::{insert_recipes_fish_type, update_fish_type};
