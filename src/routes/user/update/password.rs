4f8b633e888598d36527102f9463c158b1ba3c6e
Austin Rooks <Austionian@users.noreply.github.com>
2024-01-12 15:43:19 -0600

    temp remove current pass to reset pass

--- src/routes/user/update/password.rs
+++ src/routes/user/update/password.rs
@@ -1,4 +1,4 @@
-use crate::authentication::{validate_credentials, AuthError, Credentials};
+// use crate::authentication::{validate_credentials, AuthError, Credentials};
 use crate::utils::e500;
 use actix_web::{web, HttpResponse};
 use secrecy::{ExposeSecret, Secret};
@@ -8,7 +8,7 @@ use uuid::Uuid;
 #[derive(serde::Deserialize)]
 pub struct FormData {
     user_id: Uuid,
-    current_password: Secret<String>,
+    // current_password: Secret<String>,
     new_password: Secret<String>,
     new_password_check: Secret<String>,
 }
@@ -24,17 +24,17 @@ pub async fn change_password(
     if form.new_password.expose_secret() != form.new_password_check.expose_secret() {
         return Ok(HttpResponse::BadRequest().finish());
     }
-    let email = get_email(user_id, &pool).await.map_err(e500)?;
-    let credentials = Credentials {
-        email,
-        password: form.0.current_password,
-    };
-    if let Err(e) = validate_credentials(credentials, &pool).await {
-        return match e {
-            AuthError::InvalidCredentials(_) => Ok(HttpResponse::BadRequest().finish()),
-            AuthError::UnexpectedError(_) => Err(e500(e)),
-        };
-    }
+    // let email = get_email(user_id, &pool).await.map_err(e500)?;
+    // let credentials = Credentials {
+    //     email,
+    //     password: form.0.current_password,
+    // };
+    // if let Err(e) = validate_credentials(credentials, &pool).await {
+    //     return match e {
+    //         AuthError::InvalidCredentials(_) => Ok(HttpResponse::BadRequest().finish()),
+    //         AuthError::UnexpectedError(_) => Err(e500(e)),
+    //     };
+    // }
     crate::authentication::change_password(user_id, form.0.new_password, &pool)
         .await
         .map_err(e500)?;
@@ -42,21 +42,21 @@ pub async fn change_password(
     Ok(HttpResponse::Ok().finish())
 }
 
-async fn get_email(user_id: Uuid, pool: &PgPool) -> Result<String, sqlx::Error> {
-    let row = sqlx::query!(
-        r#"
-        SELECT email
-        FROM users
-        WHERE id = $1
-        "#,
-        user_id
-    )
-    .fetch_one(pool)
-    .await
-    .map_err(|e| {
-        tracing::error!("Failed to execute the query: {:?}", e);
-        e
-    })?;
-
-    Ok(row.email)
-}
+// async fn get_email(user_id: Uuid, pool: &PgPool) -> Result<String, sqlx::Error> {
+//     let row = sqlx::query!(
+//         r#"
+//         SELECT email
+//         FROM users
+//         WHERE id = $1
+//         "#,
+//         user_id
+//     )
+//     .fetch_one(pool)
+//     .await
+//     .map_err(|e| {
+//         tracing::error!("Failed to execute the query: {:?}", e);
+//         e
+//     })?;
+//
+//     Ok(row.email)
+// }
